bce622b044cbf279486a56c1d849885e9b6d5a89
sumitbopewar <91839519+sumitbopewar@users.noreply.github.com>
2024-05-30 18:21:14 +0530

    added firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,21 @@
-// Import the functions you need from the SDKs you need
+// // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC0uBOcSmEoDWJLjkhdd0J164cj3JmLsgo",
-  authDomain: "ecommercereact-3c539.firebaseapp.com",
-  projectId: "ecommercereact-3c539",
-  storageBucket: "ecommercereact-3c539.appspot.com",
-  messagingSenderId: "1049927114359",
-  appId: "1:1049927114359:web:26bac549a2a70ec9e8f072"
+  apiKey: "AIzaSyAU9WeqlcgHBPPcG7vovFqnqSyrdMtjs5Q",
+  authDomain: "vinzseeds.firebaseapp.com",
+  projectId: "vinzseeds",
+  storageBucket: "vinzseeds.appspot.com",
+  messagingSenderId: "1010831768147",
+  appId: "1:1010831768147:web:016b0daa30a12b6a4324a6",
+  measurementId: "G-9QWPKME9R3"
 };
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
